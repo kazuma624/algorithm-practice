512cb256ac877a7f98db19d5aba06e6692a049bd
Kazuma Hoda <saiteigundan@gmail.com>
2022-04-29 19:35:34 +0900

    3.2までやった

---
+++ chapter-2/maximum_profit_v0.py
@@ -0,0 +1,21 @@
+"""
+R_j - R_i (j < i) の最大値を求める
+
+最初の行に整数 n が与えられる。
+続く n 行に整数 R_t (t = 0,1,...,n-1) が順番に与えられる
+
+2 <= n <= 200,000
+1 <= R_t <= 10^9
+"""
+
+args = list(map(lambda x: int(x), iter(input, '')))
+n = args[0]
+R = args[1:]
+
+# O(n^2) の計算量バージョン
+maxv = R[1] - R[0]
+for j in range(1, n):
+    for i in range(j):
+        maxv = max(maxv, R[j] - R[i])
+
+print(maxv)
---
+++ chapter-2/maximum_profit_v1.py
@@ -0,0 +1,21 @@
+"""
+R_j - R_i (j < i) の最大値を求める
+
+最初の行に整数 n が与えられる。
+続く n 行に整数 R_t (t = 0,1,...,n-1) が順番に与えられる
+
+2 <= n <= 200,000
+1 <= R_t <= 10^9
+"""
+
+args = list(map(lambda x: int(x), iter(input, '')))
+n = args[0]
+R = args[1:]
+
+minv = R[0]
+maxv = -200_000_000
+for i in range(1, n):
+    maxv = max(maxv, R[i] - minv)
+    minv = min(minv, R[i])
+
+print(maxv)
