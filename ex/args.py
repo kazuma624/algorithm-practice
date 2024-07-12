2bbbad12e4618eb56b5df9236bd52953c5e415a9
Kazuma Hoda <saiteigundan@gmail.com>
2024-07-12 13:24:38 +0900

    add: 入力時の処理のメモ

---
+++ ex/args.py
@@ -0,0 +1,8 @@
+"""sys.srgv のメモ"""
+
+import sys
+
+args = sys.argv
+family_name, first_name = args[1], args[2]
+
+print(f"こんにちは、 {family_name} {first_name} さん")
---
+++ ex/input.py
@@ -0,0 +1,5 @@
+"""入力を取得するやつのメモ"""
+
+family_name = input("苗字を入力して下さい: ")
+first_name = input("名前を入力して下さい: ")
+print(f"こんにちは、 {family_name} {first_name} さん")
